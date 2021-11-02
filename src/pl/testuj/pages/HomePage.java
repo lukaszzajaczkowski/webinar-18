e48828582e23378caef510cb450ccf4bb3d1400a
Wojtek Potocki <szkolenia@wojtekpotocki.pl>
2021-11-02 18:51:18 +0100

    pop - wprowadzenie

---
+++ src/pl/testuj/pages/HomePage.java
@@ -0,0 +1,28 @@
+package pl.testuj.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePage extends BasePage {
+    public HomePage(WebDriver driver) {
+        super(driver);
+    }
+
+    public static final String WELCOME_HEADER = "Welcome to the-internet";
+    public static final String DESCRIPTION_HEADER = "Available Examples";
+
+    @FindBy(tagName = "h1")
+    private WebElement welcomeHeader;
+
+    @FindBy(tagName = "h2")
+    private WebElement descriptionHeader;
+
+    public String getWelcomeMessage() {
+        return welcomeHeader.getText();
+    }
+
+    public String getDescriptionMessage() {
+        return descriptionHeader.getText();
+    }
+}
